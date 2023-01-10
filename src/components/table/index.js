7027853d4238808f7365f6b32afb4846976385b4
amka <amarjargalbatbold@gmail.com>
2023-01-10 14:40:13 +0800

    add table index more menu path

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -1,7 +1,7 @@
 export * from './utils';
 
 export { default as useTable } from './useTable';
-
+export { default as TableMoreMenu } from './TableMoreMenu';
 export { default as TableNoData } from './TableNoData';
 export { default as TableSkeleton } from './TableSkeleton';
 export { default as TableEmptyRows } from './TableEmptyRows';
