e49336377f2d3f412d6df105d6edbc120319f723
Taivanjargal Bayarmunkh <taivanjargal.b@ictgroup.mn>
2023-01-16 11:24:01 +0800

    TableNoData

--- src/components/table/TableNoData.js
+++ src/components/table/TableNoData.js
@@ -1,10 +1,6 @@
-import PropTypes from 'prop-types';
-// @mui
-import { Table,TableBody, TableRow, TableCell } from '@mui/material';
-//
+import {  TableRow, TableCell } from '@mui/material';
 import EmptyContent from '../empty-content';
-
-// ----------------------------------------------------------------------
+import PropTypes from 'prop-types';
 
 TableNoData.propTypes = {
   isNotFound: PropTypes.bool,
@@ -12,23 +8,19 @@ TableNoData.propTypes = {
 
 export default function TableNoData({ isNotFound }) {
   return (
-    <Table>
-      <TableBody>
-        <TableRow>
-          {isNotFound ? (
-            <TableCell colSpan={12}>
-              <EmptyContent
-                title="No Data"
-                sx={{
-                  '& span.MuiBox-root': { height: 160 },
-                }}
-              />
-            </TableCell>
-          ) : (
-            <TableCell colSpan={12} sx={{ p: 0 }} />
-          )}
-        </TableRow>
-      </TableBody>
-    </Table>
+    <TableRow>
+      {isNotFound ? (
+        <TableCell colSpan={12}>
+          <EmptyContent
+            title="No Data"
+            sx={{
+              '& span.MuiBox-root': { height: 160 },
+            }}
+          />
+        </TableCell>
+      ) : (
+        <TableCell colSpan={12} sx={{ p: 0 }} />
+      )}
+    </TableRow>
   );
 }
