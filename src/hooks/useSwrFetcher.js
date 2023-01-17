ad5ce03b6fafa3ce4f71eb18473a5ae2fdac33b2
amka <amarjargalbatbold@gmail.com>
2023-01-17 18:27:22 +0800

    useSwrFetcher delete add

--- src/hooks/useSwrFetcher.js
+++ src/hooks/useSwrFetcher.js
@@ -2,12 +2,12 @@ import { useAuthContext } from 'src/auth/useAuthContext';
 
 const useSwrFetcher = () => {
   const {
-    handlers: { POST, GET },
+    handlers: { POST, GET, DELETE },
   } = useAuthContext();
 
   const postFetcher = (args) => POST(args[0], args[1] || false, args[2] || '', args[3] || 'application/json', args[4] || 'json');
   const getFetcher = (args) => GET(args[0], args[1] || false);
-  const deleteFetcher = (args) => GET(args[0], args[1] || false, args[2] || 'json');
+  const deleteFetcher = (args) => DELETE(args[0], args[1] || false, args[2] || 'json');
 
   return { postFetcher, getFetcher, deleteFetcher };
 };
