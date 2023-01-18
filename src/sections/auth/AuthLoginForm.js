4e0138c65787b91af3b4c1eae2df012d7fd024f4
amka <amarjargalbatbold@gmail.com>
2023-01-18 17:07:52 +0800

    login auth demo add

--- src/sections/auth/AuthLoginForm.js
+++ src/sections/auth/AuthLoginForm.js
@@ -1,36 +1,31 @@
+//react
 import { useState } from 'react';
-import * as Yup from 'yup';
-import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
+//mui
 import { Stack, Alert, IconButton, InputAdornment, Box } from '@mui/material';
 import { LoadingButton } from '@mui/lab';
+//named import
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { useAuthContext } from 'src/auth/useAuthContext';
-import Iconify from 'src/components/iconify';
 import FormProvider, { RHFTextField } from 'src/components/hook-form';
+import { LOGIN_URL, LOGIN_AUTH_USERNAME, LOGIN_AUTH_PASSWORD } from 'src/config-global';
+//default import
+import * as Yup from 'yup';
 import axios from 'axios';
+//components
+import Iconify from 'src/components/iconify';
 
 export default function AuthLoginForm() {
   const {
     handlers: { signIn },
   } = useAuthContext();
-
   const [showPassword, setShowPassword] = useState(false);
-
   const LoginSchema = Yup.object().shape({
-    email: Yup.string().required('Email is required').email('Email must be a valid email address'),
+    username: Yup.string().required('username is required'),
     password: Yup.string().required('Password is required'),
   });
-
-  const defaultValues = {
-    email: 'demo@minimals.cc',
-    password: 'demo1234',
-  };
-
-  const methods = useForm({
-    resolver: yupResolver(LoginSchema),
-    defaultValues,
-  });
-
+  const defaultValues = { username: '', password: '' };
+  const methods = useForm({ resolver: yupResolver(LoginSchema), defaultValues });
   const {
     reset,
     setError,
@@ -40,17 +35,15 @@ export default function AuthLoginForm() {
 
   const onSubmit = async (data) => {
     try {
-      const response = await axios.post('https://api-dev-minimal-v4.vercel.app/api/account/login', {
-        email: data.email,
-        password: data.password,
-      });
-      signIn(response.data.accessToken);
+      const response = await axios.post(
+        `${LOGIN_URL}/token?username=${data.username}&password=${data.password}&grant_type=password`,
+        {},
+        { auth: { username: LOGIN_AUTH_USERNAME, password: LOGIN_AUTH_PASSWORD } }
+      );
+      signIn(response.data.access_token);
     } catch (error) {
       reset();
-      setError('afterSubmit', {
-        ...error,
-        message: error.message,
-      });
+      setError('afterSubmit', { ...error, message: error.message });
     }
   };
 
@@ -58,7 +51,7 @@ export default function AuthLoginForm() {
     <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
       <Stack spacing={3}>
         {!!errors.afterSubmit && <Alert severity="error">{errors.afterSubmit.message}</Alert>}
-        <RHFTextField name="email" label="Email address" />
+        <RHFTextField name="username" label="username" />
         <RHFTextField
           name="password"
           label="Password"
@@ -85,10 +78,7 @@ export default function AuthLoginForm() {
         sx={{
           bgcolor: 'text.primary',
           color: (theme) => (theme.palette.mode === 'light' ? 'common.white' : 'grey.800'),
-          '&:hover': {
-            bgcolor: 'text.primary',
-            color: (theme) => (theme.palette.mode === 'light' ? 'common.white' : 'grey.800'),
-          },
+          '&:hover': { bgcolor: 'text.primary', color: (theme) => (theme.palette.mode === 'light' ? 'common.white' : 'grey.800') },
         }}
       >
         Login
