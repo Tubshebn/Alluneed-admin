347caedc40be5efaf48d48bd711e2a26851b570a
amka <amarjargalbatbold@gmail.com>
2023-01-18 15:30:00 +0800

    formfetcher add

--- src/hooks/useSwrFetcher.js
+++ src/hooks/useSwrFetcher.js
@@ -8,8 +8,11 @@ const useSwrFetcher = () => {
   const postFetcher = (args) => POST(args[0], args[1] || false, args[2] || '', args[3] || 'application/json', args[4] || 'json');
   const getFetcher = (args) => GET(args[0], args[1] || false);
   const deleteFetcher = (args) => DELETE(args[0], args[1] || false, args[2] || 'json');
-
-  return { postFetcher, getFetcher, deleteFetcher };
+  const formFetcher = (url, { arg }) => {
+    let arr = [url, true, arg.body, arg.type || 'application/json'];
+    return postFetcher(arr);
+  };
+  return { postFetcher, getFetcher, deleteFetcher, formFetcher };
 };
 
 export default useSwrFetcher;
