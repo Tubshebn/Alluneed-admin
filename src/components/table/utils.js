b77ff439b00a3fb689718ed5f5b6ebbea1b878bf
amka <amarjargalbatbold@gmail.com>
2023-01-23 11:07:46 +0800

    table skelton add

--- src/components/table/utils.js
+++ src/components/table/utils.js
@@ -1,19 +1,26 @@
 export function emptyRows(page, rowsPerPage, arrayLength) {
-  return page > 0 ? Math.max(0, (1 + page) * rowsPerPage - arrayLength) : 0;
+    return page > 0 ? Math.max(0, (1 + page) * rowsPerPage - arrayLength) : 0;
 }
 
 function descendingComparator(a, b, orderBy) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
+    if (b[orderBy] < a[orderBy]) {
+        return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+        return 1;
+    }
+    return 0;
 }
 
 export function getComparator(order, orderBy) {
-  return order === 'desc'
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
+    return order === 'desc' ? (a, b) => descendingComparator(a, b, orderBy) : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+export function labelDisplayedRows(data, title) {
+    const { count, to } = data;
+    return (
+        <span>
+            {title} <span>{count !== -1 ? count : `${to}-аас илүү`}</span>
+        </span>
+    );
 }
