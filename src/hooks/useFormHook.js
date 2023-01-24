6dd23731d3df2a09f9347f56a9d244b8e4b85e0e
amka <amarjargalbatbold@gmail.com>
2023-01-24 18:08:54 +0800

    add useFormHook

---
+++ src/hooks/useFormHook.js
@@ -0,0 +1,27 @@
+// named import
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm, Controller } from 'react-hook-form';
+
+const useFormHook = (schema, defaultValues) => {
+    const methods = useForm({
+        resolver: yupResolver(schema),
+        defaultValues,
+        mode: 'onChange',
+    });
+    const { reset, watch, setValue, control, handleSubmit, formState } = methods;
+    const values = watch();
+    return {
+        form: {
+            Controller,
+            methods,
+            values,
+            reset,
+            watch,
+            setValue,
+            control,
+            handleSubmit,
+            formState,
+        },
+    };
+};
+export default useFormHook;
