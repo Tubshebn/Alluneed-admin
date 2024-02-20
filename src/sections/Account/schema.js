eb8dec3785c6a7d1f72930b3a8eab812c7f67e8a
Taivanjargal <s18ud1101039@nmit.edu.mn>
2024-02-20 11:08:32 +0800

    sasas

--- src/sections/Account/schema.js
+++ src/sections/Account/schema.js
@@ -4,30 +4,32 @@ import { requiredMsg, phoneRegex, phoneRegexMsg } from 'src/utils/regex';
 import * as Yup from 'yup';
 
 export function userUpdateSchema() {
-   const Value = Yup.object().shape({
-      username: Yup.string().required(requiredMsg),
-      position: Yup.string().required(requiredMsg),
-      phoneNumber: Yup.string().required(requiredMsg).matches(phoneRegex, phoneRegexMsg),
-   });
-   return Value;
+  const Value = Yup.object().shape({
+    username: Yup.string().required(requiredMsg),
+    position: Yup.string().required(requiredMsg),
+    phoneNumber: Yup.string()
+      .required(requiredMsg)
+      .matches(phoneRegex, phoneRegexMsg),
+  });
+  return Value;
 }
 
 export function passwordSchema() {
-   const Value = Yup.object().shape({
-      oldPassword: Yup.string().required(requiredMsg),
-      newPassword: Yup.string().required(requiredMsg),
-      confirmPassword: Yup.string().required(requiredMsg),
-   });
-   return Value;
+  const Value = Yup.object().shape({
+    oldPassword: Yup.string().required(requiredMsg),
+    newPassword: Yup.string().required(requiredMsg),
+    confirmPassword: Yup.string().required(requiredMsg),
+  });
+  return Value;
 }
 
 export const defaultValues = {
-   firstname: '',
-   lastname: '',
-   username: '',
-   phoneNumber: '',
-   position: '',
-   oldPassword: '',
-   newPassword: '',
-   confirmPassword: '',
+  firstname: '',
+  lastname: '',
+  username: '',
+  phoneNumber: '',
+  position: '',
+  oldPassword: '',
+  newPassword: '',
+  confirmPassword: '',
 };
